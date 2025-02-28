127a7a995617fba7a2c655f0d0fc2e6376782413
Aleksandar Maric <115831251+aleksa2315@users.noreply.github.com>
2025-02-28 12:51:44 +0100

    Update meal-plan.service.ts

--- src/app/services/meal-plan.service.ts
+++ src/app/services/meal-plan.service.ts
@@ -7,7 +7,7 @@ import { MealPlan } from '../models/meal-plan.model';
   providedIn: 'root'
 })
 export class MealPlanService {
-  private baseUrl = 'teretanaback-production.up.railway.app/api/meal-plans';
+  private baseUrl = 'https://teretanaback-production.up.railway.app/api/meal-plans';
 
   constructor(private http: HttpClient) {}
 
